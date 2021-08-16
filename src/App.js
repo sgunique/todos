61263ba3475e2f5a7c3c43f8cc7b75a502a394d7
sgunique <sanugudikandula@gmail.com>
2021-08-16 22:47:57 +0200

    Deleted private file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React from 'react';
-import FrontImg from './front.png';
-import BackImg from './back.png';
 
 const App = () => {
     return (
         <>
-            <h1> App is rendering from App.js - devserver test </h1>
             <div style={{
                 display: 'flex',
                 height: '50%',
                 flexDirection: 'column'
             }}>
-                <img src={FrontImg} style={{
-                    maxHeight: '100%',
-                    transform: 'rotate(90deg) scale(0.5)'
-                }}/>
-                <img src={BackImg} style={{
-                    maxHeight: '100%',
-                    transform: 'rotate(-90deg) scale(0.5)'
-                }}/>
             </div>
         </>
     )
